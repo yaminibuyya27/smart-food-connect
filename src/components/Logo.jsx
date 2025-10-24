f1a599727014e152f353317f4f81f3918098df7e
Yamini Buyya <yaminibuyya27@gmail.com>
2025-10-24 15:51:40 -0400

    Add App Logo and Icon

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,8 @@
+import SmartFoodConnectLogo from '../assets/smart-food-connect-logo.png'
+
 const Logo = ({ onClick }) => (
-    <svg width="200" height="50" viewBox="0 0 150 50" xmlns="http://www.w3.org/2000/svg" onClick={onClick} style={{ cursor: 'pointer' }}>
-        <style>
-            {`
-        .logo-text { font-family: Arial, sans-serif; font-weight: bold; }
-        .smart { fill: #ff8000ff; }
-        .food { fill: #4CAF50; }
-        .connect { fill: #2196F3; }
-      `}
-        </style>
-        <text x="10" y="35" className="logo-text smart">Smart</text>
-        <text x="60" y="35" className="logo-text food">Food</text>
-        <text x="105" y="35" className="logo-text connect">Connect</text>
-    </svg>
+    <img src={SmartFoodConnectLogo} alt="Smart Food Connect Logo" 
+        onClick={onClick} style={{cursor: 'pointer', width: '25%'}}/>
 );
 
 export default Logo;
