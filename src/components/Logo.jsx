35f73c7815314d8d21a3acb9e1417659075322dd
Yamini Buyya <yaminibuyya27@gmail.com>
2025-11-15 12:59:02 -0500

    Logo Size change to make it responsive across all devices

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,12 @@
 import SmartFoodConnectLogo from '../assets/smart-food-connect-logo.png'
 
 const Logo = ({ onClick }) => (
-    <img src={SmartFoodConnectLogo} alt="Smart Food Connect Logo" 
-        onClick={onClick} style={{cursor: 'pointer', width: '25%'}}/>
+    <img
+        src={SmartFoodConnectLogo}
+        alt="Smart Food Connect Logo"
+        onClick={onClick}
+        className="cursor-pointer w-48 sm:w-56 md:w-64 lg:w-72"
+    />
 );
 
 export default Logo;
